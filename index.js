648386c1798f05ddaa3b8bc9a8ce874bbcb7a74c
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-07-10 19:03:55 +0100

    feat: backend

--- index.js
+++ index.js
@@ -24,31 +24,31 @@ let notes = [
     important: true,
   },
 ];
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
+// app.get("/", (request, response) => {
+//   response.send("<h1>Hello World!</h1>");
+// });
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
 });
 
-// app.get("/api/notes", (request, response) => {
-//   response.json(notes);
-// });
-
-// app.get("/api/notes/:id", (request, response) => {
-//   const id = Number(request.params.id);
-//   const note = notes.find((note) => note.id === id);
-
-//   if (note) {
-//     response.json();
-//   } else {
-//     response.status(404).end();
-//   }
-// });
-
-app.delete("/api/notes/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+  const note = notes.find((note) => note.id === id);
 
-  response.status(204).end();
+  if (note) {
+    response.json();
+  } else {
+    response.status(404).end();
+  }
 });
+
+// app.delete("/api/notes/:id", (request, response) => {
+//   const id = Number(request.params.id);
+//   notes = notes.filter((note) => note.id !== id);
+
+//   response.status(204).end();
+// });
 
 const PORT = 3001;
 

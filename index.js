6c12bffae734ed967167614863fccfb6b167a181
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-07-10 17:50:43 +0100

    feat: backend

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // console.log("hello world");
+// const http = require("http");
 
-const http = require("http");
+const express = require("express");
+const app = express();
+
 let notes = [
   {
     id: 1,
@@ -21,9 +24,12 @@ let notes = [
     important: true,
   },
 ];
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes));
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
 });
 
 const PORT = 3001;

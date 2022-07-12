154abe0f68df99e6565c2ac02b1f333c55ba16cf
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-07-12 15:21:54 +0100

    feat: backend

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-// let notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     date: "2022-05-30T17:30:31.098Z",
-//     important: true,
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only Javascript",
-//     date: "2022-05-30T18:39:34.091Z",
-//     important: false,
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     date: "2022-05-30T19:20:14.298Z",
-//     important: true,
-//   },
-// ];
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2022-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2022-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
 // app.get("/", (request, response) => {
 //   response.send("<h1>Hello World!</h1>");
 // });
@@ -52,8 +52,15 @@ app.use(express.json());
 // });
 
 app.post("/api/notes", (request, response) => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+
   const note = request.body;
-  console.log(note);
+  note.id = maxId + 1;
+
+  console.log(maxId);
+
+  notes = notes.concat(note);
+
   response.json(note);
 });
 

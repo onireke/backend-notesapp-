26662743eb4002dbddac6c62259eb0d70b4f18da
amosa abdulfatah olayimka <amosaabdulfatah@gmail.com>
2022-07-13 14:20:00 +0100

    feat: heroku-app

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// console.log("hello world");
-// const http = require("http");
-
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 
 let notes = [
@@ -25,47 +24,58 @@ let notes = [
     important: true,
   },
 ];
-// app.get("/", (request, response) => {
-//   response.send("<h1>Hello World!</h1>");
-// });
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
-// app.get("/api/notes", (request, response) => {
-//   response.json(notes);
-// });
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
 
-// app.get("/api/notes/:id", (request, response) => {
-//   const id = Number(request.params.id);
-//   const note = notes.find((note) => note.id === id);
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
 
-//   if (note) {
-//     response.json();
-//   } else {
-//     response.status(404).end();
-//   }
-// });
+  if (note) {
+    response.json();
+  } else {
+    response.status(404).end();
+  }
+});
 
-// app.delete("/api/notes/:id", (request, response) => {
-//   const id = Number(request.params.id);
-//   notes = notes.filter((note) => note.id !== id);
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
 
-//   response.status(204).end();
-// });
+  response.status(204).end();
+});
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
 app.post("/api/notes", (request, response) => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  const body = request.body;
 
-  const note = request.body;
-  note.id = maxId + 1;
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
 
-  console.log(maxId);
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
 
   notes = notes.concat(note);
 
   response.json(note);
 });
 
-const PORT = 3001;
-
-app.listen(PORT);
-
-console.log(`server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
